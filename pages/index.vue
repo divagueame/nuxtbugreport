f3af81c050606787fd2db758e159bb8ca93bf8cc
Martin Arce <martin.arce.teixeira@gmail.com>
2024-07-12 12:49:11 +0200

    Add report

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Index</h1>
+    <NuxtLink to="/login">Login</NuxtLink>
+    <br><br>
+    <button @click="testNavigate">Test Bug</button>
+  </div>
+</template>
+<script lang="ts" setup>
+const ROUTE_NAMES = {
+  LOGIN: 'login',
+};
+const testNavigate = () => {
+  navigateTo({ name: ROUTE_NAMES.LOGIN }, { open: { target: '_blank' } });
+};
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
+
